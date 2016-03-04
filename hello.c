c3925ddc83bc1bd1747ab6f3cd1bd0dd948fed9e
Arpitha Sama <asama@my.okcu.edu>
2016-03-03 19:04:50 -0600

    more modifications

--- hello.c
+++ hello.c
@@ -3,4 +3,6 @@ main()
 printf("THis is my first command in git \n");
 printf("My newest 2nd modification \n");
 printf(" I have committed the second version of the file \n");
+printf("new Lines \n");
 }
+
---
+++ myprog.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+main()
+{
+int number=10;
+for(int i=1;i<number;i++)
+{
+for(int j=i;j<=i;j++)
+printf("$");
+printf("\n");
+return 0;
+}
+}
+
+
